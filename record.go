0d6fe48b557e168ab2a581b39f64cf248519a6f2
Aleksandr Mogilnikov <zorion79@gmail.com>
2019-06-21 15:35:36 +1000

    added fields to PartResults

--- record.go
+++ record.go
@@ -22,29 +22,49 @@ type Record struct {
 			Bcc            []string `json:"bcc"`
 			Subject        string   `json:"subject"`
 		} `json:"messageInfo"`
-		Rules                []int    `json:"rules"`
-		AvStatus             string   `json:"avStatus"`
-		DocWithMacroDetected bool     `json:"docWithMacroDetected"`
-		AvNotScannedReason   string   `json:"avNotScannedReason"`
-		AsStatus             string   `json:"asStatus"`
-		AsNotScannedReason   string   `json:"asNotScannedReason"`
-		MaStatus             string   `json:"maStatus"`
-		MaNotScannedReason   string   `json:"maNotScannedReason"`
-		ApStatus             string   `json:"apStatus"`
-		ApNotScannedReason   string   `json:"apNotScannedReason"`
-		CfStatus             string   `json:"cfStatus"`
-		CfNotScannedReason   string   `json:"cfNotScannedReason"`
-		KtStatus             string   `json:"ktStatus"`
-		KtNotScannedReason   string   `json:"ktNotScannedReason"`
-		KtSkipReason         string   `json:"ktSkipReason"`
-		AvMessageSizeLimit   string   `json:"avMessageSizeLimit"`
-		AsMessageSizeLimit   string   `json:"asMessageSizeLimit"`
-		AsMethod             string   `json:"asMethod"`
-		KtProceededBy        string   `json:"ktProceededBy"`
-		ApuMethod            string   `json:"apuMethod"`
-		WmufMethod           string   `json:"wmufMethod"`
-		PartResults          []string `json:"partResults"`
-		MaInfo               struct {
+		Rules                []int  `json:"rules"`
+		AvStatus             string `json:"avStatus"`
+		DocWithMacroDetected bool   `json:"docWithMacroDetected"`
+		AvNotScannedReason   string `json:"avNotScannedReason"`
+		AsStatus             string `json:"asStatus"`
+		AsNotScannedReason   string `json:"asNotScannedReason"`
+		MaStatus             string `json:"maStatus"`
+		MaNotScannedReason   string `json:"maNotScannedReason"`
+		ApStatus             string `json:"apStatus"`
+		ApNotScannedReason   string `json:"apNotScannedReason"`
+		CfStatus             string `json:"cfStatus"`
+		CfNotScannedReason   string `json:"cfNotScannedReason"`
+		KtStatus             string `json:"ktStatus"`
+		KtNotScannedReason   string `json:"ktNotScannedReason"`
+		KtSkipReason         string `json:"ktSkipReason"`
+		AvMessageSizeLimit   string `json:"avMessageSizeLimit"`
+		AsMessageSizeLimit   string `json:"asMessageSizeLimit"`
+		AsMethod             string `json:"asMethod"`
+		KtProceededBy        string `json:"ktProceededBy"`
+		ApuMethod            string `json:"apuMethod"`
+		WmufMethod           string `json:"wmufMethod"`
+		PartResults          []struct {
+			FileName string `json:"fileName"`
+			FileSize string `json:"fileSize"`
+			AvInfo   struct {
+				Statuses []struct {
+					AvStatus string `json:"avStatus"`
+				} `json:"statuses"`
+				DocWithMacroDetected bool     `json:"docWithMacroDetected"`
+				SkipReason           string   `json:"skipReason"`
+				SkipDescription      string   `json:"skipDescription"`
+				Threats              []string `json:"threats"`
+				DisinfectedObjects   []string `json:"disinfectedObjects"`
+				DeletedObjects       []string `json:"deletedObjects"`
+			} `json:"avInfo"`
+			CfInfo struct {
+				Statuses         []string `json:"statuses"`
+				BannedFileName   string   `json:"bannedFileName"`
+				BannedFileFormat string   `json:"bannedFileFormat"`
+			} `json:"cfInfo"`
+			Action string `json:"action"`
+		} `json:"partResults"`
+		MaInfo struct {
 			DmarcVerdict string   `json:"dmarcVerdict"`
 			SpfVerdict   string   `json:"spfVerdict"`
 			DkimVerdicts []string `json:"dkimVerdicts"`
